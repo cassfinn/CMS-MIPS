88a674b35415bb6de383adb5b2aa68d9aef8a5c1
Claudia Cassidy <claudiacassidy@claudias-imac.cfl.rr.com>
2016-09-23 14:58:02 -0400

    Initial Commit

---
+++ CMS-MIPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMS-MIPS
+//
+//  Created by Claudia Cassidy on 9/23/16.
+//  Copyright © 2016 Claudia Cassidy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
